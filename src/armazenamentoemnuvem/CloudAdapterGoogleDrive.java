b20a2d2b3dab7b42989364499b7aaf8018bcf3f4
Leolucasm <leolucasm@edu.univali.br>
2016-06-02 13:35:12 -0300

    Implementacao GoogleDrive

--- src/armazenamentoemnuvem/CloudAdapterGoogleDrive.java
+++ src/armazenamentoemnuvem/CloudAdapterGoogleDrive.java
@@ -1,22 +1,24 @@
 package armazenamentoemnuvem;
 
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleBrowserClientRequestUrl;
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.googleapis.auth.oauth2.GoogleTokenResponse;
-import com.google.api.client.http.FileContent;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.DriveRequestInitializer;
 import com.google.api.services.drive.DriveScopes;
 import control.CloudAdapter;
 import java.awt.Desktop;
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
@@ -37,21 +39,27 @@ public class CloudAdapterGoogleDrive implements CloudAdapter {
         GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(httpTransport, jsonFactory, CLIENT_ID, CLIENT_SECRET, Arrays.asList(DriveScopes.DRIVE)).setAccessType("online").setApprovalPrompt("auto").build();
 
         String urlAuthorization = flow.newAuthorizationUrl().setRedirectUri(REDIRECT_URI).build();
-        try {            
+        try {
             Desktop desktop = Desktop.getDesktop();
             URI uri = new URI(urlAuthorization);
-            desktop.browse(uri);
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//            desktop.browse(uri);
+//            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-            String code = br.readLine();            
-//            GoogleTokenResponse response = new GoogleTokenResponse();
-//            response.setAccessToken("4/1d7n-hCMdcaiQqdAgkjFUlaRwckx5XiG-yH1Z9vkiY8");               
-            GoogleTokenResponse response = flow.newTokenRequest(code).setRedirectUri(REDIRECT_URI).execute();
+//            String code = br.readLine();            
+            GoogleTokenResponse response = new GoogleTokenResponse();
+            response.setAccessToken("4/1d7n-hCMdcaiQqdAgkjFUlaRwckx5XiG-yH1Z9vkiY8");
+//            GoogleTokenResponse response = flow.newTokenRequest(code).setRedirectUri(REDIRECT_URI).execute();
             GoogleCredential credential = new GoogleCredential().setFromTokenResponse(response);
-
-            Drive service = new Drive.Builder(httpTransport, jsonFactory, credential).build();
-            service.getServicePath();
-
+            Drive.Builder driveBuilder = new Drive.Builder(httpTransport, jsonFactory, credential);
+            driveBuilder.setApplicationName("sistemaArmazenamentoNuvem");            
+//            Drive service = new Drive.Builder(httpTransport, jsonFactory, credential).build();
+            OutputStream directorio  = new FileOutputStream("saida.txt");;
+            Drive service = driveBuilder.build();                        
+            Drive.Files.List request = service.files().list();
+            Drive.Files.Get getFile = service.files().get("1");
+            getFile.executeAndDownloadTo(directorio);
+            request.get("Mecanica Deno.JPG");
+            System.out.println(request.size());
 
         } catch (URISyntaxException | IOException ex) {
             ex.printStackTrace();
